8abfa58b621ed7bc38b9ed651f87dc725e1b3699
Tanmayyy209 <tanmaypatil4518@gmail.com>
2024-03-08 23:33:18 +0530

    Initial Commit

---
+++ friends/app/javascript/application.js
@@ -0,0 +1,10 @@
+
+
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require rails-ujs
+//= require_tree .
+
+
+
+import "@hotwired/turbo-rails"
+import "controllers"
